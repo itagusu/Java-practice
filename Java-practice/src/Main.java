8f4001968c5a208c8ae6a38101ab0629727534cd
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-20 18:20:46 +0900

    java練習

--- Java-practice/src/Main.java
+++ Java-practice/src/Main.java
@@ -6,12 +6,56 @@
 //	}
 //}
 
+//public class Main{
+//	public static void main(String[] args) {
+//		System.out.println("15+16を計算します");
+//		System.out.println(15 + 16);
+//		int x;
+//		x = 6;
+//		System.out.println(x * x *3.14);
+//	}
+//}
+
+//public class Main{
+//	public static void main(String[] args) {
+//		//整数型int age　年齢を入れる変数20で定義
+//		int age = 20;
+//		System.out.println("板楠の年齢は" + age + "です。");
+//		//ageに最大入27
+//		age = 27;
+//		System.out.println("いいえ、本当の年齢は" + age + "です。");
+//	}
+//}
+
+//public class Main{
+//	public static void main(String[] args) {
+//		//final は定義後変更されることがない場合に記述　例 円周率等
+//		final double pi = 3.14;
+//		int pie = 5;
+//		System.out.println(pie * pie * pi);
+//		System.out.println("半径を変更します");
+//		pie = 10;
+//		System.out.println("半径" + pie +"の円の面積は");
+//		System.out.println(pie * pie * pi + "です。");
+//	}
+//}
+
+//public class Main{
+//	public static void main(String[] args) {
+//		int a = 3;
+//		int b = 5;
+//		int c = a * b;
+//		System.out.println("縦幅" + a + "横幅" + b + "の長方形位の面積は、" + c + "です。");
+//	}
+//}
+
 public class Main{
-	public static void main(String[] args) {
-		System.out.println("15+16を計算します");
-		System.out.println(15 + 16);
-		int x;
-		x = 6;
-		System.out.println(x * x *3.14);
+	public static void main(String[] args){
+		boolean result = true;
+		char name = '駆';
+		final double pi = 3.14;
+		long number = 314159265853979L;
+		String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+		System.out.println(name + pi + number + message);
 	}
 }
