3d8d77e0455c6a4ca8774412c3d442c45fffeae4
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-20 17:09:30 +0900

    java練習

--- Java-practice/src/Main.java
+++ Java-practice/src/Main.java
@@ -1,4 +1,17 @@
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("板楠亮");
+//		System.out.println("27歳");
+//		System.out.println("熊本県");
+//	}
+//}
 
-public class Main {
-
+public class Main{
+	public static void main(String[] args) {
+		System.out.println("15+16を計算します");
+		System.out.println(15 + 16);
+		int x;
+		x = 6;
+		System.out.println(x * x *3.14);
+	}
 }
