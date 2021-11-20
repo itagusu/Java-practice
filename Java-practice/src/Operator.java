2056c74060e584f67bee3037b2a263a6610796a0
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-20 22:37:31 +0900

    java 演算子練習

---
+++ Java-practice/src/Operator.java
@@ -0,0 +1,51 @@
+//
+//public class Operator {
+//	public static void main(String[] args) {
+//		int a = 20;
+//		int b = a + 5;
+//		System.out.println(a);
+//		System.out.println(b);
+//	}
+//}
+
+//public class Operator{
+//	public static void main(String[] args) {
+//		System.out.println("私の好きな記号は二重引用符(\")です");
+//	}
+//}
+
+//public class Operator{
+//	public static void main(String[] args) {
+//		//本来int(整数型)に入らないdouble(少数型)を強制的に入れる
+//		int age = (int)3.8;
+//		System.out.println(age);
+//	}
+//}
+
+//public class Operator{
+//	public static void main(String[] args) {
+//		double d = 5.0;
+//		int i = 2;
+//		System.out.println(d / i);
+//	}
+//}
+
+//public class Operator{
+//	public static void main(String[] args) {
+//		double d = 8.8 / 2;
+//		long l = 5 + 2L;
+//		String msg = "私の年齢は" + 23;
+//		System.out.println(d);
+//		System.out.println(l);
+//		System.out.println(msg);
+//	}
+//}
+
+//命令実行の文
+public class Operator{
+	public static void main(String[] args) {
+		String name = "亮";
+		String message = name + "さん、こんにちは";
+		System.out.println(message);
+	}
+}
