b1fdef1ff47ecfe265d26bfa6af468a9b80f6084
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-22 00:08:52 +0900

    java for文練習

--- Java-practice/src/Practice3.java
+++ Java-practice/src/Practice3.java
@@ -20,32 +20,72 @@
 //	}
 //}
 
-public class Practice3{
-	public static void main(String[] args) {
-		System.out.println("あなたの運勢を占います");
-		int fortune = new java.util.Random().nextInt(4);
-//		//switch文に書き換えることができる
-//		if (fortune == 0) {
-//			System.out.println("大吉");
-//		}else if (fortune == 1) {
-//			System.out.println("中吉");
-//		}else if (fortune == 2) {
-//			System.out.println("吉");
-//		}else {
-//			System.out.println("凶");
+//public class Practice3{
+//	public static void main(String[] args) {
+//		System.out.println("あなたの運勢を占います");
+//		int fortune = new java.util.Random().nextInt(4);
+////		//switch文に書き換えることができる
+////		if (fortune == 0) {
+////			System.out.println("大吉");
+////		}else if (fortune == 1) {
+////			System.out.println("中吉");
+////		}else if (fortune == 2) {
+////			System.out.println("吉");
+////		}else {
+////			System.out.println("凶");
+////		}
+//		switch(fortune) {
+//			case 0:
+//				System.out.println("大吉");
+//				break;
+//			case 1:
+//				System.out.println("中吉");
+//				break;
+//			case 2:
+//				System.out.println("吉");
+//				break;
+//			default:
+//				System.out.println("凶");
 //		}
-		switch(fortune) {
-			case 0:
-				System.out.println("大吉");
-				break;
-			case 1:
-				System.out.println("中吉");
-				break;
-			case 2:
-				System.out.println("吉");
-				break;
-			default:
-				System.out.println("凶");
-		}
-	}
-}
+//	}
+//}
+
+//public class Practice3{
+//	public static void main(String[] args) {
+//		System.out.println("あなたの運勢を占います");
+//		int fortune = new java.util.Random().nextInt(5) + 1;
+//		switch(fortune) {
+//			case 1:
+//			case 2:
+//				System.out.println("いいね");
+//				break;
+//			case 3:
+//				System.out.println("普通");
+//				break;
+//			case 4:
+//			case 5:
+//				System.out.println("うーん・・・");
+//		}
+//	}
+//}
+
+//public class Practice3{
+//	public static void main(String[] args) {
+//		for (int i = 0; i < 20; i++) {
+//			System.out.println(i);
+//		}
+//	}
+//}
+
+//public class Practice3{
+//	public static void main(String[] args) {
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 1; j < 10; j++) {
+//				System.out.print(i * j);
+//				System.out.print(" "); //空白
+//			}
+//		System.out.println(" "); //改行
+//		}
+//	}
+//}
+
