8312103ee8616cc6ccb0025eb53cd5b750b1502b
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-24 21:29:49 +0900

    4章練習問題

--- Java-practice/src/Practice4.java
+++ Java-practice/src/Practice4.java
@@ -41,31 +41,89 @@
 //	}
 //}
 
+//public class Practice4{
+//	public static void main(String[] args) {
+//		int[] seq = new int[10];
+//		for (int i = 0; i < seq.length; i++) {
+//			seq[i] = new java.util.Random().nextInt(4);
+//		}
+//		for (int i = 0; i < seq.length; i++) {
+////			switch (seq[i]) {
+////			case 0:
+////				System.out.println("A");
+////				break;
+////			case 1:
+////				System.out.println("T");
+////				break;
+////			case 2:
+////				System.out.println("G");
+////				break;
+////			case 3:
+////				System.out.println("C");
+////				break;
+////			}
+//			//51~64行目を以下の２行で記載ができる
+//			char[] base = {'A', 'T', 'G', 'C'};
+//			System.out.print(base[seq[i]] + " ");
+//		}
+//	}
+//}
+//
+
+//public class Practice4{
+//	public static void main(String[] args) {
+//		int[] scores = {20, 40, 60, 80, 100};
+////		for (int i = 0; i < scores.length; i++) {
+//		//拡張for文
+//		for (int value : scores) {
+////			System.out.println(scores[i]);
+//			System.out.println(value);
+//		}
+//	}
+//}
+
+//public class Practice4{
+//	public static void main(String[] args) {
+//	int[] array = {1,2,3};
+//	array[0] = 10;
+//	System.out.println(array[0]);
+//	array = null;
+//	}
+//}
+
+////練習問題4-1
+//int[] points = new points[4];
+//double[] weights = new weights[5];
+//boolean[] answers = new answers[3];
+//String[] names = new names[3];
+
+////練習問題4-2
+//public class Practice4{
+//	public static void main(String[] args) {
+//		int[] moneyList = {121902, 8302, 55100};
+//		for (int i = 0; i < moneyList.length; i++) {
+//			System.out.println(moneyList[i]);
+//		}
+//	for (int m : moneyList) {
+//		System.out.println(m);
+//		}
+//	}
+//}
+
+//練習問題4-4
 public class Practice4{
 	public static void main(String[] args) {
-		int[] seq = new int[10];
-		for (int i = 0; i < seq.length; i++) {
-			seq[i] = new java.util.Random().nextInt(4);
-		}
-		for (int i = 0; i < seq.length; i++) {
-//			switch (seq[i]) {
-//			case 0:
-//				System.out.println("A");
-//				break;
-//			case 1:
-//				System.out.println("T");
-//				break;
-//			case 2:
-//				System.out.println("G");
-//				break;
-//			case 3:
-//				System.out.println("C");
-//				break;
-//			}
-			//51~64行目を以下の２行で記載ができる
-			char[] base = {'A', 'T', 'G', 'C'};
-			System.out.print(base[seq[i]] + " ");
+		int[] numbers = {3, 4, 9};
+		System.out.println("1桁の数字を入力してください！");
+		int input = new java.util.Scanner(System.in).nextInt();
+//		for (int n = 0; n < 3; n++) {
+//			if (input == n);
+//			System.out.println(numbers[n]);
+//		}
+		for (int n = 0; n < numbers.length; n++) {
+			if (input == numbers[n]) {
+				System.out.println("あたり");
+			}
 		}
 	}
 }
-
