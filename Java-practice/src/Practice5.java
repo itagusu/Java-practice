2389062e41028cfb2d832d074f7e30092b015592
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-25 14:51:24 +0900

    練習問題5

--- Java-practice/src/Practice5.java
+++ Java-practice/src/Practice5.java
@@ -66,34 +66,52 @@
 //}
 
 //練習問題
+//public class Practice5{
+//	public static void introduceOneself() {
+//		String name = "板楠亮";
+//		int age = 27;
+//		double tall = 172.5;
+//		char eto = '犬';
+//		System.out.println("こんにちは。名前は"+name+"と申します。歳は"+age+"歳です。"+
+//				"身長は"+tall+"cmです。"+eto+"年です。");
+//	}
+//	public static void email(String title,String address,String text) {
+//		
+//		System.out.println(address + "に、以下のメールを送信しました");
+//		System.out.println("件名:" + title);
+//		System.out.println("本文:" + text);
+//	}
+//	public static void email(String address, String text) {
+//		System.out.println(address + "に、以下のメールを送信しました");
+//		System.out.println("件名:無題");
+//		System.out.println("本文:" + text);
+//	}
+//	public static void main(String[]args) {
+////練習問題5-1
+////		introduceOneself();
+////練習問題5-2
+//		String title = "明日の件";
+//		String address = "ir060906@gmail.com";
+//		String text = "明日は予定通り１０時に自宅に向かいます。";
+//		email(title,address,text);
+//		email(address,text);
+//	}
+//}
+
 public class Practice5{
-	public static void introduceOneself() {
-		String name = "板楠亮";
-		int age = 27;
-		double tall = 172.5;
-		char eto = '犬';
-		System.out.println("こんにちは。名前は"+name+"と申します。歳は"+age+"歳です。"+
-				"身長は"+tall+"cmです。"+eto+"年です。");
+	public static void main(String[] args) {
+		double triangle = calcTriangleArea(10,5);
+		System.out.println("底辺が10.0cm 高さが5.0cmの場合 面積は" + triangle + "㎠");
+		double circle = calcCircleArea(5);
+		System.out.println("円の半径が5.0cmの場合 円の面積は" + circle + "㎠");
 	}
-	public static void email(String title,String address,String text) {
-		
-		System.out.println(address + "に、以下のメールを送信しました");
-		System.out.println("件名:" + title);
-		System.out.println("本文:" + text);
+	public static double calcTriangleArea(double bottom, double height) {
+		double triangle = bottom * height / 2;
+		return triangle;
 	}
-	public static void email(String address, String text) {
-		System.out.println(address + "に、以下のメールを送信しました");
-		System.out.println("件名:無題");
-		System.out.println("本文:" + text);
-	}
-	public static void main(String[]args) {
-//練習問題5-1
-//		introduceOneself();
-//練習問題5-2
-		String title = "明日の件";
-		String address = "ir060906@gmail.com";
-		String text = "明日は予定通り１０時に自宅に向かいます。";
-		email(title,address,text);
-		email(address,text);
+	public static double calcCircleArea(double radios) {
+		double π = 3.14;
+		double circle = radios * radios * π;
+		return circle;
 	}
 }
