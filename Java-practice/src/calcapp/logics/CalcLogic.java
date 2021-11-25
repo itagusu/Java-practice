d930f3322d4571776dc0ff94277ff224a426ab5c
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-25 20:55:35 +0900

    パッケージ

---
+++ Java-practice/src/calcapp/logics/CalcLogic.java
@@ -0,0 +1,13 @@
+package calcapp.logics;
+
+public class CalcLogic {
+	public static double tasu(double x, double y) {
+		double total = x + y;
+		return total;
+	}
+	public static double hiku(double x, double y) {
+		double delta = x - y;
+		return delta;
+	}
+
+}
---
+++ Java-practice/src/calcapp/main/Calc.java
@@ -0,0 +1,11 @@
+package calcapp.main;
+import calcapp.logics.CalcLogic;
+public class Calc {
+	public static void main(String[] args) {
+		//FQCN出なくてもエラーにならない　importの記述をしたため
+		double total = CalcLogic.tasu(5,3);
+		//FQCNで書いても問題はなく動作する
+		double delta = CalcLogic.hiku(5,3); 
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+}
