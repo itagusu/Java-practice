b10846d15f5770695cb8254861bb600d6b9b62ad
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-27 15:44:29 +0900

    インスタンス作成

--- Java-practice/src/Practice8.java
+++ Java-practice/src/Practice8.java
@@ -1,4 +1,15 @@
 
 public class Practice8 {
-
+	public static void main(String[]args) {
+		//1.勇者を生成
+		Hero h = new Hero();
+		h.name = "ハル";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "が誕生しました");
+		
+		h.sit(5);
+		h.slip();
+		h.sit(25);
+		h.run();
+	}
 }
