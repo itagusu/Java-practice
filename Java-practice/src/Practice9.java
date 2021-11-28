9bcf00f5ef59fb6a4948df689a8f6ccc6a52a79e
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-28 21:30:12 +0900

    剣を２つ

--- Java-practice/src/Practice9.java
+++ Java-practice/src/Practice9.java
@@ -9,11 +9,17 @@ public class Practice9 {
 		h2.name = "ゆうり";
 		h2.hp = 120;
 		
-		Sword s = new Sword();
-		s.name = "秋水";
-		s.damage = 10;
-		h1.sw = s;
-		System.out.println("現在の武器は" + h1.sw.name);
+		Sword s1 = new Sword();
+		s1.name = "秋水";
+		s1.damage = 10;
+		h1.sw = s1;
+		System.out.println(h1.name + "の現在の武器は" + h1.sw.name);
+		
+		Sword s2 = new Sword();
+		s2.name = "閻魔";
+		s2.damage = 15;
+		h2.sw = s2;
+		System.out.println(h2.name + "の現在の武器は" + h2.sw.name);
 		
 		Wizard w = new Wizard();
 		w.name = "りょう";
@@ -21,6 +27,7 @@ public class Practice9 {
 		System.out.println(w.name + "のHPは" + w.hp);
 		
 		h1.attack();
+		h2.attack();
 		w.heal(h1);
 		w.heal(h2);
 	}
