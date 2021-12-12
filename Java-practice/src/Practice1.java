a793cd0f523a13a0cc33de6e21d6ad4fba8a6ecc
itagusu <88267436+itagusu@users.noreply.github.com>
2021-12-12 18:02:47 +0900

    練習問題1_5

---
+++ Java-practice/src/Practice1.java
@@ -0,0 +1,16 @@
+
+public class Practice1 {
+	public static void main(String[]args) {
+//		int a = 3;
+//		int b = 5;
+//		int c = a*b;
+//		System.out.println("縦３横５の長方形の面積は" + c);
+		
+		boolean result = true;
+		char name = '駆';
+		final double pi = 3.14;
+		long number = 314159265853979L;
+		String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+		System.out.println(name + pi + number + message);
+	}
+}
