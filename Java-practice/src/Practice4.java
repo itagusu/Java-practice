0528303dff56b25aaf095705a49f3d4565a05667
itagusu <88267436+itagusu@users.noreply.github.com>
2021-12-20 16:32:16 +0900

    練習問題4

--- Java-practice/src/Practice4.java
+++ Java-practice/src/Practice4.java
@@ -113,6 +113,18 @@
 //練習問題4-4
 public class Practice4{
 	public static void main(String[] args) {
+		practice4_2();
+	}
+	public static void practice4_2() {
+		int[] moneyList = {121902,8302,55100};
+		for(int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+		for(int m : moneyList) {
+			System.out.println(m);
+		}
+	}
+	public static void practice4_4() {
 		int[] numbers = {3, 4, 9};
 		System.out.println("1桁の数字を入力してください！");
 		int input = new java.util.Scanner(System.in).nextInt();
--- Java-practice/src/track/App.java
+++ Java-practice/src/track/App.java
@@ -27,39 +27,83 @@ public class App {
 //     return lines.toArray(new String[lines.size()]);
   }
   public static void example1(){
-	double a = 1000;
-    int b = 600;
-    int c = 250;
-    int d = 100;
-    LocalTime BaseTime = LocalTime.of(22, 00, 00);
-    //LocalTime time1 = LocalTime.of(10, 25, 00);
-    LocalTime time2 = LocalTime.of(23, 40, 00);
-    int distance1 = 1500;
-    double distance2 = 2001;
-    // trueの場合1.2倍
-    //boolean t1 = BaseTime.isBefore(time1);
+	double a = 1000; //基本料金内距離
+    int b = 600; //定額料金
+    int c = 250; //250mあたり追加料金
+    int d = 100; //追加料金(250m毎)
+    LocalTime BaseTime = LocalTime.of(22, 00, 00); //料金変動時間
+    LocalTime time1 = LocalTime.of(10, 25, 00); //1人目乗車時間
+    LocalTime time2 = LocalTime.of(23, 40, 00); //2人目乗車時間
     boolean t2 = BaseTime.isBefore(time2);
-    System.out.println(t2);
-    double AddDistance1 = distance1 - a;
-    double AddDistance2 = distance2 - a;
-    double AddCharge1 = Math.ceil(AddDistance1/c);
-    double AddCharge2 = Math.ceil(AddDistance2/c);
+    double[] distance = {1500,2001}; //配列で記述
     
-    int total1 = b + (int)Math.ceil(AddDistance1/c) * d;
-    int total2 = b + (int)Math.ceil(AddDistance2/c) * d;
-    System.out.println(total1);
-    System.out.println(total2);
-    System.out.println(Math.round(total1 + (total2 * 1.2)));
     
-    if (t2 == true) {
-    	
-    } else {
+    double[] AddDistance = distance - a;
+    int total = b + (int)Math.ceil(AddDistance/c) * d;
+    for(int i =0;i < distance.length; i++) {
+    	if(t2 == true) {
+    		total * 1.2;
+    	}else
+    		total;
     	
     }
+    	System.out.println(total);
+    
+//    int distance1 = 1500;
+//    double distance2 = 2001;
+    // trueの場合1.2倍
+    boolean t1 = BaseTime.isBefore(time1);
+    boolean t2 = BaseTime.isBefore(time2);
+//    System.out.println(t2);
+//    double AddDistance1 = distance1 - a;
+//    double AddDistance2 = distance2 - a;
+//    double AddCharge1 = Math.ceil(AddDistance1/c);
+//    double AddCharge2 = Math.ceil(AddDistance2/c);
+//    
+//    int total1 = b + (int)Math.ceil(AddDistance1/c) * d;
+//    int total2 = b + (int)Math.ceil(AddDistance2/c) * d;
+//    System.out.println(total1);
+//    System.out.println(total2);
+//    System.out.println(Math.round(total1 + (total2 * 1.2)));
+//    
+//    if (t2 == true) {
+//    	
+//    } else {
+//    	
+//    }
     	
 //    int z = b + 
 //    System.out.println(z);
     
+    
+//    public static void example1(){
+//    	double a = 1000;
+//        int b = 600;
+//        int c = 250;
+//        int d = 100;
+//        LocalTime BaseTime = LocalTime.of(22, 00, 00);
+//        //LocalTime time1 = LocalTime.of(10, 25, 00);
+//        LocalTime time2 = LocalTime.of(23, 40, 00);
+//        int distance1 = 1500;
+//        double distance2 = 2001;
+//        // trueの場合1.2倍
+//        //boolean t1 = BaseTime.isBefore(time1);
+//        boolean t2 = BaseTime.isBefore(time2);
+//        double AddDistance1 = distance1 - a;
+//        double AddDistance2 = distance2 - a;
+//        double AddCharge1 = Math.ceil(AddDistance1/c);
+//        double AddCharge2 = Math.ceil(AddDistance2/c);
+//        
+//        int total1 = b + (int)Math.ceil(AddDistance1/c) * d;
+//        int total2 = b + (int)Math.ceil(AddDistance2/c) * d;
+//        System.out.println(Math.round(total1 + (total2 * 1.2)));
+//        
+//        if (t2 == true) {
+//        	
+//        } else {
+//        	
+//        }
+//      }
     
     
   }
