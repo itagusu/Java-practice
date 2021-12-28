a7bb4c11eb1a3f054e58ede9496e76cee09c1f20
itagusu <88267436+itagusu@users.noreply.github.com>
2021-12-28 12:35:50 +0900

    ソフト工房

---
+++ Java-practice/src/Soft3.java
@@ -0,0 +1,25 @@
+
+public class Soft3 {
+	public static void main(String[]args) {
+//		soft3_1();
+		soft3_2();
+	}
+	public static void soft3_1() {
+		int x = new java.util.Scanner(System.in).nextInt();
+		int y = new java.util.Scanner(System.in).nextInt();
+		if (x > y) {
+			System.out.println("xはyより大きい");
+		}else {
+			System.out.println("xはyより小さい");
+		}
+	}
+	public static void soft3_2() {
+		int x = new java.util.Scanner(System.in).nextInt();
+		int y = new java.util.Scanner(System.in).nextInt();
+		if (x > y) {
+			System.out.println(x);
+		}else {
+			System.out.println(y);
+		}
+	}
+}
